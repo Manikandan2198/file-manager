77457007cc59afbc843a56624a648eb26a6a6c6c
Manikandan <manikandan2198@gmail.com>
2021-07-19 20:03:10 +0530

    initial json change commit

--- src/common/initialFileSystem.js
+++ src/common/initialFileSystem.js
@@ -7,43 +7,8 @@ const InitialFileSystem = {
       date: '2019-04-07',
       parentPath: null,
       parentID: null,
-      children: [
-        '8b5ede9f0ce35d5c67017c9909c6db40',
-        'ee06a1a0dfe7895206de290785da2c6a'
-      ]
-    },
-    '8b5ede9f0ce35d5c67017c9909c6db40': {
-        type: 'folder',
-        name: 'apps',
-        size: 0,
-        date: '2019-04-29',
-        parentID: 'ea278d3e246754e8da1163a8e6517c08',
-        parentPath: 'root/',
-        path: 'root/apps/',
-        children: [
-            '9fde21762e0054565b756ca454bf4f36'
-        ]
-      },
-      'ee06a1a0dfe7895206de290785da2c6a': {
-        type: 'folder',
-        name: 'pictures',
-        size: 0,
-        date: '2019-04-29',
-        parentID: 'ea278d3e246754e8da1163a8e6517c08',
-        parentPath: 'root/',
-        path: 'root/pictures/',
-        children: []
-      },
-      '9fde21762e0054565b756ca454bf4f36':{
-        type: 'folder',
-        name: 'vscode',
-        size: 0,
-        date: '2019-04-29',
-        parentID: '8b5ede9f0ce35d5c67017c9909c6db40',
-        parentPath: 'root/apps/',
-        path: 'root/apps/vscode/',
-        children: []
-      }
+      children: []
+    }
   };
   
 
